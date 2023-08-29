488bce593f5454adf0aea200edf580fd14439117
triprjt <triprjt@gmail.com>
2023-08-30 00:18:41 +0530

    changes in DB. changes in URL

--- grade5/urls.py
+++ grade5/urls.py
@@ -8,10 +8,14 @@ urlpatterns = [
     path('chapter_modules/<int:chapter_id>/', views.getModulesInAChapter),
     path('module_content/<int:module_id>/', views.get_content_in_module, name='get_content_in_module'),
     
+    # udpate progress of text, image, video, MCQ content 
     path('update_progress_of_content/<int:module_id>/<str:content_type>/', views.update_progress_of_content),
 
-    path('update_module_progress/<int:module_id>', views.update_module_progress),
-    path('update_chapter_progress/<int:chapter_id>', views.update_chapter_progress),
-    path('update_subject_progress/<int:subject_id>', views.update_subject_progress),
+    # update progress of module, chapter, subject
+    path('update_module_progress/<int:module_id>/', views.update_module_progress),
+    path('update_chapter_progress/<int:chapter_id>/', views.update_chapter_progress),
+    path('update_subject_progress/<int:subject_id>/', views.update_subject_progress),
+    
+    # login
     path('api-token-auth/', obtain_auth_token , name='api_token_auth'),
 ]
