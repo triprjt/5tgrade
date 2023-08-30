d0abf8559b86ca1093684c4ae8552ae8cc343a21
triprjt <triprjt@gmail.com>
2023-08-30 15:37:49 +0530

    removed Dockerfile and added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+ web: gunicorn 'grade5.wsgi'
--- backend/settings.py
+++ backend/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = "django-insecure-8r4ej-=1_7hnu$m&@lb1mp3(8rt9#nie*qaxr!87d9o1va1=@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
-
+ALLOWED_HOST = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -135,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "static/"
+# STATIC_ROOT = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
