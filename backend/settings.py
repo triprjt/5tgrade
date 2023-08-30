3d4082afeb9841b727416cb3c367d032e6062299
triprjt <triprjt@gmail.com>
2023-08-30 16:26:22 +0530

    cors Allow All updated

--- backend/settings.py
+++ backend/settings.py
@@ -147,11 +147,12 @@ REST_FRAMEWORK = {
                 ],
 }
 
-CORS_ALLOW_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8000",
-    '*',
-]
+CORS_ALLOW_ALL_ORIGINS= True
+# CORS_ALLOW_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "http://127.0.0.1:8000",
+#     '*',
+# ]
 
 # Access-Control-Allow-Origin = ['*']
